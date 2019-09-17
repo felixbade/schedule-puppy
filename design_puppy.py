92595438e39757f0aac4961d70e88559c5cb15aa
Felix Bade <flix.bade@gmail.com>
2019-09-17 19:50:21 +0300

    Add formatting for whole events

--- design_puppy.py
+++ design_puppy.py
@@ -7,6 +7,7 @@ import requests
 from secret import calendar_url
 
 calendar = Calendar(requests.get(calendar_url).text)
+events_on = Timeline(calendar).on(arrow.now().shift(days=5))
 
 
 def arrow_to_local_str(time):
@@ -18,9 +19,17 @@ def event_time_to_tg(event):
         event_time += '–' + arrow_to_local_str(event.end)
     return event_time
 
-events_on = Timeline(calendar).on(arrow.now().shift(days=5))
+def event_to_tg(event):
+    text = event_time_to_tg(event)
+    if event.location:
+        text += '\n'
+        text += '📍 '
+        text += event.location
+    text += '\n'
+    text += '👉 '
+    text += event.name
+    return text
+
 for event in events_on:
-    print('name:', event.name)
-    print('location:', event.location)
-    print('time:', event_time_to_tg(event))
+    print(event_to_tg(event))
     print()
